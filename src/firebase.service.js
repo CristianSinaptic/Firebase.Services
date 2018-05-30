556e69af9fa0acaaa72d7d281884338b3ff3376e
CristianSinaptic <cdominguez@sinaptic.com.ar>
2018-05-30 15:45:39 -0300

    Commit inicial

---
+++ src/firebase.service.js
@@ -0,0 +1,49 @@
+//////////////////////////////////////
+// FIREBASE SERVICES
+//////////////////////////////////////
+var admin = require("firebase-admin");
+var os = require('os');
+var osu = require('os-utils');
+
+function initService(serviceAccount, databaseUrl){
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        databaseURL: databaseUrl
+      });
+      
+}
+
+function setPresence (serverName){
+    var db = admin.database();
+    db.ref('serverStatus/' + serverName).set({status:'online'});
+    console.log("initPresence - OK");
+}
+
+
+function setMetrics(serverName){
+    osu.cpuUsage(function(cpu){
+        var m = { 'cpu': { 'usage': Math.round(cpu * 100,2) }, 'memory': { 'total': Math.round(os.totalmem() /1024/1024/1024, 2), 'available': Math.round(os.freemem() /1024/1024/1024, 2) }};
+        var db = admin.firestore();
+        db.collection('serverInfo').doc(serverName).set({metrics: m}, {merge: true});
+        console.log("setMetrics - OK");
+    });    
+}
+
+function firebase(){
+    console.log("Firebase constructor");
+}
+
+firebase.prototype.initPresence = function(serverName) {
+    initPresence(serverName);
+}
+
+firebase.prototype.setPresence = function(serviceAccount, databaseUrl) {
+    setPresence(serviceAccount, databaseUrl);
+}
+
+
+firebase.prototype.setMetrics = function(serverName) {
+    setMetrics(serverName);
+}
+
+module.exports = firebase;
